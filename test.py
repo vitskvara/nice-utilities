e6188864f2819e34ec05955300d773a3f7007d91
Vit Skvara <g2skvit@r040c03s03.marconi.cineca.it>
2017-11-07 17:44:28 +0100

    added testing data

---
+++ test.py
@@ -0,0 +1,4 @@
+import util
+
+
+test = util.VacTHManip(
--- util.py
+++ util.py
@@ -1,2 +1,11 @@
 class VacTHManip:
-	INPUT_FOLDER 
+	input_folder = ""
+	output_folder = ""
+	data_folder = ""
+	run_folder = ""
+	
+	def __init__(self, data_folder_, run_folder_):
+		data_folder = data_folder_
+		run_folder = run_folder_
+		input_folder = data_folder + "/input"
+		output_folder = data_folder + "/output"
