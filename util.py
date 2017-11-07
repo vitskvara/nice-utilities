42bd9d9e642ecd2a2eaeef2cd1ed246ae3da9816
Vit Skvara <g2skvit@r040c03s03.marconi.cineca.it>
2017-11-07 18:15:34 +0100

    tests

--- util.py
+++ util.py
@@ -2,15 +2,59 @@ import numpy as np
 import pylab as pl
 
 class VacTHManip:
-	input_folder = ""
-	output_folder = ""
-	data_folder = ""
-	run_folder = ""
+	""" class for VacTH/NICE input/output manipulation and running """
+	_input_folder = ""
+	_output_folder = ""
+	_data_folder = ""
+	_run_folder = ""
 	
 	def __init__(self, data_folder_, run_folder_):
-		data_folder = data_folder_
-		run_folder = run_folder_
-		input_folder = data_folder + "/input"
-		output_folder = data_folder + "/output"
+		self._data_folder = data_folder_
+		self._run_folder = run_folder_
+		self._input_folder = data_folder_ + "/input"
+		self._output_folder = data_folder_ + "/output"
 		
-def load_txt
+		data = np.zeros(5)
+	
+	def getDataFolder(self):
+		return self._data_folder
+		
+def save2DArray(X, file):
+	""" saves 2D numpy array with shape stored in the first line """
+	shape = X.shape
+	with open(file, 'w') as f:
+		f.write(str(shape[0]) + '\n')
+		f.write(str(shape[1]) + '\n')
+		np.savetxt(f, X)
+		f.close()
+		
+def load2DArray(file, verb = False):
+	""" load array saved with previous function or differently in the same format """
+	with open(file, 'r') as f:
+		n = int(f.readline())
+		m = int(f.readline())
+		
+		if verb:
+			print("loading array of size %d x %d " % (n,m))
+		
+		X = np.loadtxt(f)
+	return X
+
+def save1DArray(X, file):
+	""" saves 2D numpy array with shape stored in the first line """
+	shape = X.shape
+	with open(file, 'w') as f:
+		f.write(str(shape[0]) + '\n')
+		np.savetxt(f, X)
+		f.close()
+		
+def load1DArray(file, verb = False):
+	""" load array saved with previous function or differently in the same format """
+	with open(file, 'r') as f:
+		n = int(f.readline())
+		
+		if verb:
+			print("loading array of size %d " % (n))
+		
+		X = np.loadtxt(f)
+	return X
